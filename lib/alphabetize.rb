ff7e3f84982ad2b8c84fda67d521ccd48d1a2e0b
Adam Kearney <aklives@hotmail.com>
2019-03-24 07:49:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
-def alphabetize(arr)
-  # code here
+
+def alphabetize(phrases)
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  hash = Hash.new
+  esperanto_split = ESPERANTO_ALPHABET.split("").to_a
+  esperanto_split.each_with_index do |letter, idx|
+    for i in 0..phrases.length-1
+      if phrases[i].chr == letter
+        hash[phrases[i]] = idx
+      end
+    end
+  end
+  array = Array.new
+  hash.each do |phrase, index|
+    array << phrase
+  end
+
+array
 end
