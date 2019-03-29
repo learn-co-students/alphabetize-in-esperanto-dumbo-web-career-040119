f8234e5cdad324e569526b429735bb5a74991899
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 16:41:10 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,9 @@
-ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+  arr.sort_by do |a|
+    a.split("").map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
