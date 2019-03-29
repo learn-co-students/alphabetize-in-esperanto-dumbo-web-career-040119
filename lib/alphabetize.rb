5cd19b2f660a7c52a11f65ee0ce49cf2b6897347
cogilvy <chrisogilvy@cox.net>
2019-03-29 13:07:28 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(array)
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  array.sort_by do |word|
+    word.split("").collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
